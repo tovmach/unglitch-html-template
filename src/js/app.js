3e7624dffad082a5172dbe76fa5acdc5b16b333c
Michael <michaeltovmach.noreply@gmail.com>
2022-08-04 16:20:58 +0200

    feat: js refactored

--- src/js/app.js
+++ src/js/app.js
@@ -1,134 +1,164 @@
 /* Description: Custom JS file */
 
-window.onscroll = function () {
-  scrollFunction()
-  scrollFunctionBTT() // back to top button
+/*=================================
+   JavaScript INDEX
+===================================
+    1. Onload and Onscroll
+    2. Navigation 
+    3. Skills
+    4. Testimonial Swiper
+    5. Back To Top Button
+    6. Custom Cursor
+    7. Circular Text 
+
+
+ =================================*/
+
+// use strict is on
+'use strict'
+
+/********************/
+/*     1. Onload    */
+/********************/
+window.onload = () => {
+  unglitch_navigation()
+  unglitch_skills_tabs_change_years()
+  unglitch_testimonial_swiper()
+  unglitch_back_to_top_button()
+  unglitch_cutom_cursor()
+  unglitch_circular_text()
 }
 
-window.onload = function () {
-  scrollFunction()
-  customCursor()
-}
+/********************************/
+/*       2. Navigation          */
+/********************************/
 
-/******************************/
-/*        Navigation          */
-/******************************/
-const mainNavBar = document.getElementById('mainNavBar')
+const unglitch_navigation = () => {
+  const mainNavBar = document.getElementById('mainNavBar')
 
-// Collapse the navbar by adding the navbar-scrolled class
-function scrollFunction() {
-  if (document.documentElement.scrollTop > 1) {
-    mainNavBar.classList.add('navbar-scrolled')
-  } else if (document.documentElement.scrollTop < 1) {
-    mainNavBar.classList.remove('navbar-scrolled')
-  }
-}
+  // Collapse the navbar by adding the navbar-scrolled class
+  window.addEventListener('scroll', () => {
+    if (document.documentElement.scrollTop > 1) {
+      mainNavBar.classList.add('navbar-scrolled')
+    } else if (document.documentElement.scrollTop < 1) {
+      mainNavBar.classList.remove('navbar-scrolled')
+    }
+  })
 
-// Hide main nav on scroll down show on scroll up
-document.addEventListener('DOMContentLoaded', function () {
-  // add padding-top to body (if necessary)
+  // Hide main nav on scroll down show on scroll up
   const navbarHeight = mainNavBar.offsetHeight
-  document.body.style.paddingTop = navbarHeight + 'px'
+  let lastScrollTop = 0
 
-  if (mainNavBar) {
-    let lastScrollTop = 0
-    window.addEventListener('scroll', function () {
-      let scrollTop = window.scrollY
-      /* Let navbarHeight px scroll before hiding the main nav */
-      if (document.documentElement.scrollTop > navbarHeight) {
-        if (scrollTop < lastScrollTop) {
-          /* add the navbar-scrolled-up class to main nav and removes navbar-scrolled-down */
-          mainNavBar.classList.remove('navbar-scrolled-down')
-          mainNavBar.classList.add('navbar-scrolled-up')
-        } else {
-          /* add the navbar-scrolled-down class to main nav and removes navbar-scrolled-up */
-          mainNavBar.classList.remove('navbar-scrolled-up')
-          mainNavBar.classList.add('navbar-scrolled-down')
-        }
-        lastScrollTop = scrollTop
+  window.addEventListener('scroll', () => {
+    let scrollTop = window.scrollY
+    /* Let navbarHeight px scroll before hiding the main nav */
+    if (document.documentElement.scrollTop > navbarHeight) {
+      if (scrollTop < lastScrollTop) {
+        /* add the navbar-scrolled-up class to main nav and removes navbar-scrolled-down */
+        mainNavBar.classList.remove('navbar-scrolled-down')
+        mainNavBar.classList.add('navbar-scrolled-up')
+      } else {
+        /* add the navbar-scrolled-down class to main nav and removes navbar-scrolled-up */
+        mainNavBar.classList.remove('navbar-scrolled-up')
+        mainNavBar.classList.add('navbar-scrolled-down')
       }
+      lastScrollTop = scrollTop
+    }
+  })
+
+  // Close offcanvas on click from mobile menu
+  const bsOffcanvas = new bootstrap.Offcanvas('#offcanvasNavbar')
+  const navUl = document.getElementById('navlist')
+  const navLiList = navUl.children
+  for (const li of navLiList) {
+    li.addEventListener('click', () => {
+      setTimeout(() => bsOffcanvas.hide(), 500)
     })
   }
-})
-
-//Close offcanvas from mobile menu
-const bsOffcanvas = new bootstrap.Offcanvas('#offcanvasNavbar')
-const navUl = document.getElementById('navlist')
-const navLiList = navUl.children
-for (const li of navLiList) {
-  li.addEventListener('click', () => {
-    setTimeout(() => bsOffcanvas.hide(), 500)
-  })
 }
 
 /**********************/
-/*       Swiper Testimonial       */
+/*      3. Skills     */
 /**********************/
-var swiper = new Swiper('#testimonialSwiper', {
-  slidesPerView: 1,
+const unglitch_skills_tabs_change_years = () => {
+  const yearsOfExperience = document.querySelector(
+    '.years-of-experience__number'
+  )
+  const skills = document.querySelectorAll('.skill')
 
-  loop: true,
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-  },
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-})
-/**********************/
-/*      Skills       */
-/**********************/
-const yearsOfExperience = document.querySelector('.years-of-experience__number')
-const skills = document.querySelectorAll('.skill')
+  const firstSkillYears = skills[0].dataset.years
+  yearsOfExperience.textContent = firstSkillYears
 
-const firstSkillYears = skills[0].dataset.years
-yearsOfExperience.textContent = firstSkillYears
+  for (const skill of skills) {
+    skill.addEventListener('click', () => {
+      const years = skill.dataset.years
 
-for (const skill of skills) {
-  skill.addEventListener('click', () => {
-    const years = skill.dataset.years
-
-    yearsOfExperience.textContent = years
-  })
-}
-
-/******************************/
-/*     Back To Top Button     */
-/******************************/
-// Get the button
-myButton = document.getElementById('backToTopButton')
-
-// When the user scrolls down 20px from the top of the document, show the button
-function scrollFunctionBTT() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    myButton.classList.add('back-to-top-button__show')
-  } else {
-    myButton.classList.remove('back-to-top-button__show')
+      yearsOfExperience.textContent = years
+    })
   }
 }
 
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0 // for Safari
-  document.documentElement.scrollTop = 0 // for Chrome, Firefox, IE and Opera
+/************************************/
+/*      4. Testimonial Swiper       */
+/************************************/
+// initiate the testimonial swiper
+const unglitch_testimonial_swiper = () => {
+  new Swiper('#testimonialSwiper', {
+    slidesPerView: 1,
+
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev'
+    }
+  })
 }
 
-/******************************/
-/*       Custom Cursor        */
-/******************************/
-// add js-link class to swiper.js buttons to enable cursor interactions
-// *** need to put this code before seting cursor
-const swiperElements = document.querySelectorAll(
-  '.swiper-button-next, .swiper-pagination-bullet, .swiper-button-prev'
-)
-for (const element of swiperElements) {
-  element.classList.add('js-link')
-}
+/*********************************/
+/*     5. Back To Top Button     */
+/*********************************/
 
+const unglitch_back_to_top_button = () => {
+  // Get the button
+  const myButton = document.getElementById('backToTopButton')
+
+  // When the user scrolls down 20px from the top of the document, show the button
+  window.addEventListener('scroll', () => {
+    if (
+      document.body.scrollTop > 20 ||
+      document.documentElement.scrollTop > 20
+    ) {
+      myButton.classList.add('back-to-top-button__show')
+    } else {
+      myButton.classList.remove('back-to-top-button__show')
+    }
+  })
+
+  // When the user clicks on the button, scroll to the top of the document
+  myButton.addEventListener('click', () => {
+    document.body.scrollTop = 0 // for Safari
+    document.documentElement.scrollTop = 0 // for Chrome, Firefox, IE and Opera
+  })
+}
+/*********************************/
+/*       6. Custom Cursor        */
+/*********************************/
 // set custom cursor
-function customCursor() {
+function unglitch_cutom_cursor() {
+  // add js-link class to swiper.js buttons to enable cursor interactions
+  // *** need to put this code before seting cursor
+  const swiperElements = document.querySelectorAll(
+    '.swiper-button-next, .swiper-pagination-bullet, .swiper-button-prev'
+  )
+  for (const element of swiperElements) {
+    element.classList.add('js-link')
+  }
+
+  // custom cursor code
   const cursorInner = document.querySelector('.cursor-inner')
   const cursorOuter = document.querySelector('.cursor-outer')
   const links = document.querySelectorAll(
@@ -173,32 +203,34 @@ function customCursor() {
     })
   }
 }
-/******************************/
-/*       Circular Text        */
-/******************************/
-const list = document.querySelectorAll('.circular-text')
-for (const item of list) {
-  new CircleType(item)
-}
 
+/*********************************/
+/*       7. Circular Text        */
+/*********************************/
+const unglitch_circular_text = () => {
+  const list = document.querySelectorAll('.circular-text')
+  for (const item of list) {
+    new CircleType(item)
+  }
+}
 /******************************/
 /*           Glitch           */
 /******************************/
 
-// const glitchList = document.querySelectorAll('[class*="glitch"]')
+const glitchList = document.querySelectorAll('[class*="glitch"]')
 
-// for (const glitch of glitchList) {
-//   glitch.dataset.glitch = glitch.textContent
-// }
+for (const glitch of glitchList) {
+  glitch.dataset.glitch = glitch.textContent
+}
 
 //js library Glitched Writer
-const left__hero__title = document.querySelector('.left-hero__title')
-const text = left__hero__title.textContent
-left__hero__title.textContent = 'X'
+// const left__hero__title = document.querySelector('.left-hero__title')
+// const text = left__hero__title.textContent
+// left__hero__title.textContent = 'X'
 
-const writer = GlitchedWriter.create(left__hero__title, {
-  maxGhosts: 0.1,
-  ghostChance: 0.1,
-})
+// const writer = GlitchedWriter.create(left__hero__title, {
+//   maxGhosts: 0.1,
+//   ghostChance: 0.1
+// })
 
-writer.write(text)
+// writer.write(text)
